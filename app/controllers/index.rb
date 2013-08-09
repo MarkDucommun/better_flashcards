42784e154f104dec40d9fe72616e8d76cbb12596
Apprentice <apprentice@dbc-chicago-009.local>
2013-08-09 15:14:18 -0500

    finished controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,6 +6,44 @@ get '/' do
 end
 
 get '/profile/:user_id' do
+  @user = User.find(session[:user_id])
+  @display = "_profile"
+  @deck = Deck.all
+  erb :index
+end
 
+get '/deck/:deck_id/card/:card_id' do
+  @deck = Deck.find(params[:deck_id])
+  @card = Card.find(params[:card_id])
+  @display = "_deck"
+
+  erb :index
+end
+
+post '/submit_guess' do
+  @deck = Deck.find(params[:deck_id])
+  @card = Card.find(params[:card_id])
+  if session[:round_id]
+    @round = Round.find(session[:round_id])
+  else
+    @round = Round.create(deck: @deck, user: User.find(session[:user_id]))
+  end
+  # have round
+  @round.guesses.create(response: params[:response], card: @card)
+  
+  # figure out which redirect to do
+  if next_card = @deck.next_card_after(@card)
+    redirect "/deck/#{@deck.id}/card/#{next_card.id}"
+  else
+    session[:round_id] = nil
+    redirect "/results/#{@round.id}"
+  end
+end
+
+get '/results/:round_id' do
+  @round = Round.find(params[:round_id])
+  @display = "_results"
+
+  erb :index
 end
 
