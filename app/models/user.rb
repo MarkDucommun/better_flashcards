18b32f84845948b3632e19b720290d7f30334cff
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-09 17:34:14 -0500

    add user_methods and statistics to profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ActiveRecord::Base
   has_many :decks, through: :rounds
 
   eval(UserHelper.add_methods_to_model)
+
+  def get_undone_decks
+    Deck.all - self.decks
+  end
+
+  def get_top_rounds
+    self.rounds.order('score DESC').limit(10)
+  end
+
+  def get_recent_rounds
+    self.rounds.order('DESC').limit(5)
+  end
 end
